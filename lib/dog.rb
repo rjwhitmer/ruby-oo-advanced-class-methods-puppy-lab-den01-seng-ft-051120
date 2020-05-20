b7854bd3d540e5328f1b77ecb2d491dfad992de6
Bob Whitmer <rjwhitmercc@gmail.com>
2020-05-20 13:53:52 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # Add your code here
+require 'pry'
+class Dog
+    @@all = []
+
+    attr_reader :name
+
+    def initialize name
+        @name = name
+        
+        self.save
+    end
+    
+    def self.print_all
+        self.all.each do |dogs|
+            puts dogs.name
+        end
+    end
+
+    def save
+         @@all << self
+        #  binding.pry
+        #  self.Dog.new
+    end
+
+    
+    
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+end
